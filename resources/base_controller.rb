95b25950ed2182c91e4a68c2a8b658a9c39f682a
chloe <chloe@meisterlabs.com>
2021-07-06 16:09:19 +0200

    Allow for underscores in view names

--- resources/base_controller.rb
+++ resources/base_controller.rb
@@ -1,7 +1,9 @@
 class BaseController
   def render(view, opts={})
     require_relative "../app/views/#{controller_name}/#{view}"
-    self.class.const_get(view.capitalize).send(:new).send(:render)
+    view_formatted = view.to_s.split('_').map(&:capitalize).join
+
+    self.class.const_get(view_formatted).send(:new).send(:render)
   end
 
   private
