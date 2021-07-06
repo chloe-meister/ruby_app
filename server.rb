3aab6bb2a4ae56fd28f505049277a551141d19e0
chloe <chloe@meisterlabs.com>
2021-07-06 10:45:40 +0200

    Some routing added

--- server.rb
+++ server.rb
@@ -1,18 +1,38 @@
-# tcp_server.rb
+# frozen_string_literal: true
+
 require 'socket'
 require 'net/http'
 
 server = TCPServer.new 5678
 
+PAGES = {
+  '/' => 'Hi, welcome to the home page!',
+  '/about' => 'About us: we are http hackers',
+  '/news' => "We haven't made much news yet with this server, but stay tuned"
+}.freeze
+
+PAGE_NOT_FOUND = "Sorry, there's nothing here."
+
 while session = server.accept
   request = session.gets
   puts request
 
+  split_request = request&.split(' ')
+  path = split_request[1]
+
+  if PAGES.keys.include? path
+    status = '200 OK'
+    response_body = PAGES[path]
+  else
+    status = '404 Not Found'
+    response_body = PAGE_NOT_FOUND
+  end
+
   # Build a proper HTTP response
   response = <<~HEREDOC
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{status}
 
-    Hello world! The time is #{Time.now}
+    #{response_body}
   HEREDOC
 
   # Send it
