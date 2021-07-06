9048695066efa86174eb01474ab8bbac7de658f2
chloe <chloe@meisterlabs.com>
2021-07-06 11:17:35 +0200

    Fix typos in the html text

--- app/views/error_404.rb
+++ app/views/error_404.rb
@@ -26,12 +26,12 @@ class Error404
 
       <div class='contant_box_404'>
       <h3 class='h2'>
-        Look like you're lost
+        It looks like you're lost
           </h3>
 
-          <p>the page you are looking for not avaible!</p>
+          <p>The page you are looking for is not available!</p>
 
-          <a href='' class='link_404'>Go to Home</a>
+          <a href='/' class='link_404'>Go to Home</a>
         </div>
           </div>
           </div>
