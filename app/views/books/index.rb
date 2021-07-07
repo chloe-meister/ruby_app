4dd680602bb733493e520c61b472cdc987f85d63
chloe <chloe@meisterlabs.com>
2021-07-07 17:13:35 +0200

    Fix data retrieval

--- app/views/books/index.rb
+++ app/views/books/index.rb
@@ -6,11 +6,12 @@ class Index < BaseView
   private
 
   def html
-    response_message = "<h1> Books! </h1>"
+    response_message = '<h1> Books! </h1>'
     response_message << '<ul>'
     @data&.each do |books|
-      response_message << "<li> On <b>#{books[:date]}</b>, I bought this book: #{books[:isbn]} !</li>"
+      response_message << "<li> On <b>#{books['date']}</b>, I bought this book: #{books['isbn']} !</li>"
     end
     response_message << '</ul>'
+    response_message
   end
 end
